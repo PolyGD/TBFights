b605baeaf358e3292e0e253f9b7e0fab5dfd023d
Valdis <valdisxp1@gmail.com>
2017-10-27 22:22:40 +0200

    simple websocket

--- client/src/main/scala/ch/epfl/polygamedev/tbfights/ScalaJSExample.scala
+++ client/src/main/scala/ch/epfl/polygamedev/tbfights/ScalaJSExample.scala
@@ -2,10 +2,22 @@ package ch.epfl.polygamedev.tbfights
 
 import ch.epfl.polygamedev.tbfights.shared.SharedMessages
 import org.scalajs.dom
+import org.scalajs.dom.raw.{MessageEvent, WebSocket}
 
 object ScalaJSExample {
 
   def main(args: Array[String]): Unit = {
     dom.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
+    val wsProtocol = if (dom.document.location.protocol == "https:") "wss" else "ws"
+
+    val ws = new WebSocket(s"$wsProtocol://${dom.document.location.host}/ws")
+    ws.onmessage = {
+      event =>
+        println(event.data)
+    }
+    ws.onopen = {
+      _ =>
+        println("websocket open")
+    }
   }
 }

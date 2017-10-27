1d6de5cd97ba1ea16ce252add93e369f61f97460
Valdis <valdisxp1@gmail.com>
2017-10-27 22:22:21 +0200

    add a hello

--- server/app/ch/epfl/polygamedev/tbfights/actors/CommunicationActor.scala
+++ server/app/ch/epfl/polygamedev/tbfights/actors/CommunicationActor.scala
@@ -4,6 +4,7 @@ import akka.actor.{Actor, ActorRef, Props}
 import ch.epfl.polygamedev.tbfights.messages.{Ping, Pong}
 
 class CommunicationActor(out: ActorRef) extends Actor {
+  out ! Pong("Welcome")
   def receive = {
     case Ping(msg) =>
       out ! Pong(msg+"!")

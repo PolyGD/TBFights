bee200204098d1975e262308e94fea839812a7b0
Valdis <valdisxp1@gmail.com>
2017-11-10 14:46:02 +0100

    loaded assets

--- client/src/main/scala/ch/epfl/polygamedev/tbfights/ScalaJSExample.scala
+++ client/src/main/scala/ch/epfl/polygamedev/tbfights/ScalaJSExample.scala
@@ -2,7 +2,7 @@ package ch.epfl.polygamedev.tbfights
 
 import ch.epfl.polygamedev.tbfights.messages.{Ping, Pong}
 import ch.epfl.polygamedev.tbfights.shared.SharedMessages
-import com.definitelyscala.phaser.{Game, Phaser}
+import com.definitelyscala.phaser.{Game, Phaser, State}
 import org.scalajs.dom
 import org.scalajs.dom.raw.{HTMLButtonElement, HTMLInputElement}
 
@@ -20,7 +20,24 @@ object ScalaJSExample {
     dom.document.getElementById("send").asInstanceOf[HTMLButtonElement].onclick = {
       _ => connector ! Ping(msgBox.value)
     }
-    val phaser = new Game(500, 500, Phaser.CANVAS, "gameArea")
+    val game = new Game(500, 500, Phaser.CANVAS, "gameArea")
+    val battleState = new State {
+      override def preload(game: Game): Unit = {
+        game.load.tilemap("badMap", "versionedAssets/maps/badmap.csv") //CSV is default
+        game.load.image("placeholder","versionedAssets/images/our-art/placeholder.png")
+        game.load.image("tile-grass","versionedAssets/images/our-art/tiles/grass.png")
+      }
+
+      override def create(game: Game): Unit = {
+
+      }
+
+      override def update(game: Game): Unit = {
+
+      }
+    }
+    game.state.add("battle", battleState)
+    game.state.start("battle", clearWorld = true, clearCache = true)
   }
 
 }

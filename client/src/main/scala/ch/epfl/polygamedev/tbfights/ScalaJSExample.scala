6035d5bebbe8ba3f0e95ddef9a5dba747c40d142
Valdis <valdisxp1@gmail.com>
2017-11-10 15:48:36 +0100

    added a human

--- client/src/main/scala/ch/epfl/polygamedev/tbfights/ScalaJSExample.scala
+++ client/src/main/scala/ch/epfl/polygamedev/tbfights/ScalaJSExample.scala
@@ -2,7 +2,7 @@ package ch.epfl.polygamedev.tbfights
 
 import ch.epfl.polygamedev.tbfights.messages.{Ping, Pong}
 import ch.epfl.polygamedev.tbfights.shared.SharedMessages
-import com.definitelyscala.phaser.{Game, Phaser, State, Tilemap}
+import com.definitelyscala.phaser._
 import org.scalajs.dom
 import org.scalajs.dom.raw.{HTMLButtonElement, HTMLInputElement}
 
@@ -26,10 +26,12 @@ object ScalaJSExample {
         val TILED_JSON = 1
         game.load.tilemap("badMap", "versionedAssets/maps/badmap.json", null, TILED_JSON)
         game.load.image("placeholder", "versionedAssets/images/our-art/placeholder.png")
-        game.load.image("grass","versionedAssets/images/our-art/tiles/grass.png")
+        game.load.image("grass", "versionedAssets/images/our-art/tiles/grass.png")
+        game.load.image("human1", "versionedAssets/images/our-art/units/human1/human1.png")
       }
 
       var map: Tilemap = _
+      var troops: Seq[Sprite] = Seq.empty
 
       override def create(game: Game): Unit = {
         map = game.add.tilemap("badMap")
@@ -40,6 +42,14 @@ object ScalaJSExample {
         val layer2 = map.createLayer("Tile Layer 2")
 
         layer1.resizeWorld()
+
+        def addHumanAtTile(x: Int, y: Int) = {
+          // head starts at the tile above
+          game.add.sprite(32 * x, 32 * (y - 1), "human1")
+        }
+
+        troops :+= addHumanAtTile(1, 2)
+        troops :+= addHumanAtTile(2, 4)
       }
 
       override def update(game: Game): Unit = {

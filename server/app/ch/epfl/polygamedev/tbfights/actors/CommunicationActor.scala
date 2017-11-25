e3a1a403383f2cd84eab852dfa6fc8d3ef7047b2
Valdis <valdisxp1@gmail.com>
2017-11-25 23:45:59 +0100

    extracted battle logic to a separate actor

--- server/app/ch/epfl/polygamedev/tbfights/actors/CommunicationActor.scala
+++ server/app/ch/epfl/polygamedev/tbfights/actors/CommunicationActor.scala
@@ -1,28 +1,21 @@
 package ch.epfl.polygamedev.tbfights.actors
 
 import akka.actor.{Actor, ActorRef, Props}
-import ch.epfl.polygamedev.tbfights.battle.BattleState
 import ch.epfl.polygamedev.tbfights.messages._
 
 class CommunicationActor(out: ActorRef) extends Actor {
   out ! Pong("Welcome")
 
-  var battleState =  BattleState.example1
+  val battleActor: ActorRef = context.actorOf(BattleActor.props)
 
-  out ! BattleStarted(battleState)
-
+  battleActor ! BattleActor.Join
   def receive = {
     case Ping(msg) =>
       out ! Pong(msg + "!")
-    case MoveTroop(troopId, from, to) =>
-      val result = battleState.withMove(troopId, from, to)
-      result.fold {
-        out ! BadTroopMove(troopId, from, to)
-      } {
-        newState =>
-          battleState = newState
-          out ! TroopMoved(troopId, from, to, newState)
-      }
+    case forwarded: MoveTroop =>
+      battleActor ! forwarded
+    case forwarded: OutMessage =>
+      out ! forwarded
   }
 }
 

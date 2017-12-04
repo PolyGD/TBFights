eabdcfd01e03dc3e2960905448b2752b61644a25
Valdis <valdisxp1@gmail.com>
2017-12-04 16:54:48 +0100

    removed example code resize the area to fit the map

--- client/src/main/scala/ch/epfl/polygamedev/tbfights/ScalaJSExample.scala
+++ client/src/main/scala/ch/epfl/polygamedev/tbfights/ScalaJSExample.scala
@@ -2,28 +2,18 @@ package ch.epfl.polygamedev.tbfights
 
 import ch.epfl.polygamedev.tbfights.battle._
 import ch.epfl.polygamedev.tbfights.messages._
-import ch.epfl.polygamedev.tbfights.shared.SharedMessages
 import com.definitelyscala.phaser._
-import org.scalajs.dom
-import org.scalajs.dom.raw.{HTMLButtonElement, HTMLInputElement}
-
-import scala.scalajs.js
 
 object ScalaJSExample {
 
   def main(args: Array[String]): Unit = {
-    dom.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
 
     val connector = Connector()
     connector.listen {
       case Pong(msg) => println(s"Got ponged:$msg")
     }
 
-    val msgBox = dom.document.getElementById("msg").asInstanceOf[HTMLInputElement]
-    dom.document.getElementById("send").asInstanceOf[HTMLButtonElement].onclick = {
-      _ => connector ! Ping(msgBox.value)
-    }
-    val game = new Game(500, 500, Phaser.CANVAS, "gameArea")
+    val game = new Game(30*32, 30*32, Phaser.CANVAS, "gameArea")
     val battleState = new State {
       override def preload(game: Game): Unit = {
         val TILED_JSON = 1

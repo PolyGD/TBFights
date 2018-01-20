6c1c84804b7549edfbb8aa0cbfe996aee70cd450
Thibault Urien <thibault.urien@gmail.com>
2018-01-20 16:02:54 +0100

    multilevel tiles

--- maps/tiles.tsx
+++ maps/tiles.tsx
@@ -1,9 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="grassAndWater" tilewidth="32" tileheight="32" tilecount="24" columns="6">
- <image source="../server/public/images/our-art/tiles/grassAndWater.png" width="192" height="128"/>
+<tileset name="grassAndWater" tilewidth="32" tileheight="32" tilecount="96" columns="12">
+ <image source="../server/public/images/our-art/tiles/alltiles.png" width="384" height="256"/>
  <terraintypes>
-  <terrain name="water" tile="23"/>
-  <terrain name="grass" tile="18"/>
+  <terrain name="water" tile="41"/>
+  <terrain name="grass" tile="36"/>
+  <terrain name="viod" tile="89"/>
+  <terrain name="cliff" tile="19"/>
+  <terrain name="waterslope" tile="79"/>
  </terraintypes>
  <tile id="0" terrain="1,1,1,0"/>
  <tile id="1" terrain="1,1,0,0"/>
@@ -11,22 +14,75 @@
  <tile id="3" terrain="0,0,1,1" probability="0.3"/>
  <tile id="4" terrain="0,0,1,0"/>
  <tile id="5" terrain="1,1,0,1"/>
- <tile id="6" terrain="1,0,1,1"/>
- <tile id="7" terrain="0,0,1,1" probability="0.7"/>
- <tile id="8" terrain="0,0,1,0"/>
- <tile id="9" terrain="1,1,0,0"/>
- <tile id="10" terrain="0,1,0,0"/>
- <tile id="11" terrain="0,1,1,1"/>
- <tile id="12" terrain="1,1,1,1" probability="0.05"/>
- <tile id="13" terrain="1,1,1,1" probability="0.2"/>
- <tile id="14" terrain="1,0,1,0"/>
- <tile id="15" terrain="0,1,0,0"/>
- <tile id="16" terrain="0,0,0,1"/>
- <tile id="17" terrain="0,0,0,1"/>
- <tile id="18" terrain="1,1,1,1" probability="0.3"/>
- <tile id="19" terrain="1,1,1,1" probability="0.05"/>
- <tile id="20" terrain="1,1,1,1" probability="0.3"/>
- <tile id="21" terrain="1,0,0,0"/>
- <tile id="22" terrain="0,1,0,1"/>
- <tile id="23" terrain="0,0,0,0"/>
+ <tile id="6" terrain="1,1,1,3"/>
+ <tile id="7" terrain="1,1,3,3"/>
+ <tile id="8" terrain="1,1,3,1"/>
+ <tile id="9" terrain="3,1,3,3"/>
+ <tile id="10" terrain="1,3,3,3"/>
+ <tile id="12" terrain="1,0,1,1"/>
+ <tile id="13" terrain="0,0,1,1" probability="0.7"/>
+ <tile id="14" terrain="0,0,1,0"/>
+ <tile id="15" terrain="1,1,0,0"/>
+ <tile id="16" terrain="0,1,0,0"/>
+ <tile id="17" terrain="0,1,1,1"/>
+ <tile id="18" terrain="1,3,1,3"/>
+ <tile id="19" terrain="3,3,3,3"/>
+ <tile id="20" terrain="3,1,3,1"/>
+ <tile id="21" terrain="3,3,3,1"/>
+ <tile id="22" terrain="3,3,1,3"/>
+ <tile id="24" terrain="1,1,1,1" probability="0.05"/>
+ <tile id="25" terrain="1,1,1,1" probability="0.2"/>
+ <tile id="26" terrain="1,0,1,0"/>
+ <tile id="27" terrain="0,1,0,0"/>
+ <tile id="28" terrain="0,0,0,1"/>
+ <tile id="29" terrain="0,0,0,1"/>
+ <tile id="30" terrain="1,3,1,1"/>
+ <tile id="31" terrain="3,3,1,1"/>
+ <tile id="32" terrain="3,1,1,1"/>
+ <tile id="36" terrain="1,1,1,1" probability="0.3"/>
+ <tile id="37" terrain="1,1,1,1" probability="0.05"/>
+ <tile id="38" terrain="1,1,1,1" probability="0.3"/>
+ <tile id="39" terrain="1,0,0,0"/>
+ <tile id="40" terrain="0,1,0,1"/>
+ <tile id="41" terrain="0,0,0,0"/>
+ <tile id="48" terrain="1,1,1,2"/>
+ <tile id="49" terrain="1,1,2,2"/>
+ <tile id="50" terrain="1,2,2,2"/>
+ <tile id="51" terrain="2,2,1,1"/>
+ <tile id="52" terrain="2,2,1,2"/>
+ <tile id="53" terrain="1,1,2,1"/>
+ <tile id="54" terrain="1,0,1,2"/>
+ <tile id="55" terrain="0,0,2,2"/>
+ <tile id="56" terrain="0,1,2,1"/>
+ <tile id="57" terrain="0,1,2,2"/>
+ <tile id="58" terrain="1,0,2,2"/>
+ <tile id="60" terrain="1,2,1,1"/>
+ <tile id="61" terrain="2,2,1,1"/>
+ <tile id="62" terrain="2,2,1,2"/>
+ <tile id="63" terrain="1,1,2,2"/>
+ <tile id="64" terrain="2,1,2,2"/>
+ <tile id="65" terrain="2,1,1,1"/>
+ <tile id="66" terrain="1,4,1,0"/>
+ <tile id="67" terrain="3,3,4,3"/>
+ <tile id="68" terrain="4,1,0,1"/>
+ <tile id="69" terrain="3,3,3,4"/>
+ <tile id="72" terrain="1,1,1,1"/>
+ <tile id="73" terrain="1,1,1,1"/>
+ <tile id="74" terrain="1,2,1,2"/>
+ <tile id="75" terrain="2,1,2,2"/>
+ <tile id="76" terrain="2,2,2,1"/>
+ <tile id="77" terrain="2,2,2,1"/>
+ <tile id="78" terrain="3,4,1,4"/>
+ <tile id="80" terrain="4,3,4,1"/>
+ <tile id="81" terrain="3,3,3,4"/>
+ <tile id="82" terrain="3,3,4,3"/>
+ <tile id="84" terrain="1,1,1,1"/>
+ <tile id="85" terrain="1,1,1,1"/>
+ <tile id="86" terrain="1,1,1,1"/>
+ <tile id="87" terrain="1,2,2,2"/>
+ <tile id="88" terrain="2,1,2,1"/>
+ <tile id="89" terrain="2,2,2,2"/>
+ <tile id="90" terrain="1,4,0,0"/>
+ <tile id="91" terrain="4,4,0,0"/>
+ <tile id="92" terrain="4,1,0,0"/>
 </tileset>

2d393e896c55777bb4beec57df66feef9830b954
Thibault Urien <thibault.urien@gmail.com>
2018-01-21 18:25:50 +0100

    improve human 1

---
+++ maps/clifs.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="clifs" tilewidth="32" tileheight="32" tilecount="24" columns="6">
+ <image source="../our-art/tiles/clifs.png" width="192" height="128"/>
+ <terraintypes>
+  <terrain name="high_ground" tile="7"/>
+  <terrain name="low_ground" tile="3"/>
+ </terraintypes>
+ <tile id="0" terrain="1,1,1,0"/>
+ <tile id="1" terrain="1,1,0,0"/>
+ <tile id="2" terrain="1,1,0,1"/>
+ <tile id="3" terrain="1,1,1,1"/>
+ <tile id="6" terrain="1,0,1,0"/>
+ <tile id="7" terrain="0,0,0,0"/>
+ <tile id="8" terrain="0,1,0,1"/>
+ <tile id="12" terrain="1,0,1,1"/>
+ <tile id="13" terrain="0,0,1,1"/>
+ <tile id="14" terrain="0,1,1,1"/>
+</tileset>
